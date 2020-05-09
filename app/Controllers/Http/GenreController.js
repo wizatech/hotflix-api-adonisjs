b6c52a46b15f93c06d94dbcf26808ac19348b0d8
wizatek <diogown3d@gmail.com>
2020-05-09 18:28:18 -0400

    Finish CRUD Genres

--- app/Controllers/Http/GenreController.js
+++ app/Controllers/Http/GenreController.js
@@ -3,25 +3,23 @@ const GenreService = use('App/Services/GenreService')
 
 class GenreController {
   async index ({ request, response, view }) {
-    return response.json(await GenreService.getAllGenre())
-  }
-
-  async create ({ request, response, view }) {
+    return response.json(await GenreService.all())
   }
 
   async store ({ request, response }) {
+    return response.json(await GenreService.store(request))
   }
 
   async show ({ params, request, response, view }) {
-  }
-
-  async edit ({ params, request, response, view }) {
+    return response.json(await GenreService.show(params.id))
   }
 
   async update ({ params, request, response }) {
+    return response.json(await GenreService.update(params.id, request))
   }
 
   async destroy ({ params, request, response }) {
+    return response.json(await GenreService.destroy(params.id))
   }
 }
 
--- app/Services/GenreService.js
+++ app/Services/GenreService.js
@@ -1,9 +1,48 @@
 'use strict'
 const Genre = use('App/Models/Genre')
-
+const Logger = use('Logger')
 class GenreService {
-  static async getAllGenre() {
-    return await Genre.all()
+  static async all() {
+    try {
+      return await Genre.all()
+    } catch (error) {
+      Logger.error(error)
+    }
+  }
+
+  static async store(request) {
+    try {
+      return await Genre.create(request.only(["name", "description"]))
+    } catch (error) {
+      Logger.error(error)
+    }
+  }
+
+  static async show(id) {
+    try {
+      return await Genre.findOrFail(id)
+    } catch (error) {
+      Logger.error(error)
+    }
+  }
+
+  static async update(id, request) {
+    try {
+      const genre = await Genre.findOrFail(id)
+      genre.merge(request.only(["name", "description"]))
+      return genre.save()
+    } catch (error) {
+      Logger.error(error)
+    }
+  }
+
+  static async destroy(id) {
+    try {
+      const genre = await Genre.findOrFail(id)
+      return await genre.delete()
+    } catch (error) {
+      Logger.error(error)
+    }
   }
 }
 
